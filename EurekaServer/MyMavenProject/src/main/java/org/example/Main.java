ad29059f99f267e40a9575a9a4bbf53ddbd71f9b
Korimi Praveen <korimi.praveen@slksoftware.com>
2025-11-25 12:55:23 +0530

    Added eureka-server and gateway-service

---
+++ EurekaServer/MyMavenProject/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        SpringApplication.run(Main.class, args);
+        System.out.println("Application started...");
+    }
+}
